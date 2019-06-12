194b1e466948f10a23b145bdb07ac96fe9e439ed
Ksenia Shirokova <xebusk@mail.ru>
2019-06-12 20:21:24 +0300

    1-4

--- src/main/java/com/example/planets/GreetingController.java
+++ src/main/java/com/example/planets/GreetingController.java
@@ -8,14 +8,21 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class GreetingController {
     @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="name", required = false, defaultValue = "world") String name, Model model){
-        model.addAttribute("name", name);
+    public String greeting(@RequestParam(name="myname", required = false, defaultValue = "world") String name, Model model){
+        model.addAttribute("myname", name);
         return "greeting";
     }
-
-    @GetMapping
-    public String main(Model model) {
-        model.addAttribute("some", "o lets do something ");
-        return "main";
+    @GetMapping("/mercury")
+    public void mercury(Model model) {
+    }
+    @GetMapping("/venus")
+    public void venus(Model model) {
+    }
+    @GetMapping("/earth")
+    public String earth(Model model) {
+        return "earth";
+    }
+    @GetMapping("/mars")
+    public void mars(Model model) {
     }
 }
