39d6a01c2ce6ee6d3d96a8a0b619981b35bdadfa
Ksenia Shirokova <xebusk@mail.ru>
2019-07-03 17:24:34 +0300

    greeting deleted

--- src/main/java/com/example/planets/GreetingController.java
+++ src/main/java/com/example/planets/GreetingController.java
@@ -3,25 +3,22 @@ package com.example.planets;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class GreetingController {
-    @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="myname", required = false, defaultValue = "world") String name, Model model){
-        model.addAttribute("myname", name);
-        return "greeting";
-    }
     @GetMapping("/mercury")
     public void mercury(Model model) {
     }
+
     @GetMapping("/venus")
     public void venus(Model model) {
     }
+
     @GetMapping("/earth")
     public String earth(Model model) {
         return "earth";
     }
+
     @GetMapping("/mars")
     public void mars(Model model) {
     }
